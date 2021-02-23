54ed303e84edecd75a755397ec9ea15721bbb645
Симуков Андрей <zzokter@gmail.com>
2021-02-23 20:53:59 +1000

    add weapon with basic effects and scripts. add an opponent as a target to check

---
+++ Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+    public float health = 50f;
+
+    //the function receives damage from the weapon script as input
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            fknDie(); 
+        }
+    }
+
+    public void fknDie()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera FPScam;
+
+    public ParticleSystem muzzleFlash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(FPScam.transform.position + FPScam.transform.forward/2, FPScam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            damageTransfer(hit);
+        }
+    }
+
+    void damageTransfer(RaycastHit hit)
+    {
+        EnemyHit enemy = hit.transform.GetComponent<EnemyHit>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+    }
+}
